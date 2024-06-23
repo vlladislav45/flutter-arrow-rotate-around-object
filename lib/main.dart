db4358ffcfbd07c11be09238ae703b0cb5cc0299
vladislavenev <vlladislav45@gmail.com>
2024-06-23 21:53:33 +0300

    feat: improved arrow direction to point inward

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class ArrowPainter extends CustomPainter {
       ..color = Colors.red
       ..style = PaintingStyle.fill;
 
-    final double arrowSize = 40.0;
+    final double arrowSize = 20.0;
     final Offset center = Offset(size.width / 2, size.height / 2);
     final double radius = size.width / 2 - arrowSize;
 
@@ -81,24 +81,36 @@ class ArrowPainter extends CustomPainter {
       center.dy + radius * sin(adjustedAngle),
     );
 
-    // Draw the arrow with the desired shape
+    // Draw the arrow
     drawArrow(canvas, paint, arrowPosition, arrowSize, adjustedAngle);
+
+    // Draw the center image
+    final centerImage = AssetImage('assets/images/motorcycle.png');
+    centerImage.resolve(ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        final imageSize = Size(info.image.width.toDouble(), info.image.height.toDouble());
+        final Rect src = Offset.zero & imageSize;
+        final Rect dst = Rect.fromCenter(center: center, width: 60, height: 60);
+        canvas.drawImageRect(info.image, src, dst, Paint());
+      }),
+    );
   }
 
   void drawArrow(Canvas canvas, Paint paint, Offset position, double size, double angle) {
     final Path path = Path();
     path.moveTo(position.dx, position.dy);
+    path.lineTo(position.dx + size, position.dy - size / 2);
     path.lineTo(position.dx + size / 2, position.dy - size / 2);
-    path.lineTo(position.dx + size / 2, position.dy - size / 4);
-    path.lineTo(position.dx + size, position.dy - size / 4);
-    path.lineTo(position.dx + size, position.dy + size / 4);
-    path.lineTo(position.dx + size / 2, position.dy + size / 4);
+    path.lineTo(position.dx + size / 2, position.dy - size / 2);
+    path.lineTo(position.dx - size / 2, position.dy);
     path.lineTo(position.dx + size / 2, position.dy + size / 2);
+    path.lineTo(position.dx + size / 2, position.dy + size / 2);
+    path.lineTo(position.dx + size, position.dy + size / 2);
     path.close();
 
     canvas.save();
     canvas.translate(position.dx, position.dy);
-    canvas.rotate(angle + pi); // Rotate the arrow to point inward
+    canvas.rotate(angle); // Rotate the arrow
     canvas.translate(-position.dx, -position.dy);
     canvas.drawPath(path, paint);
     canvas.restore();
